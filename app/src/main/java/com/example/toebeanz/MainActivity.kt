b9bda26b58593150341033cdf424403a23785285
Hannah <hannahstone4@hotmail.com>
2020-09-28 11:01:49 -0300

    Working on game part of app

--- app/src/main/java/com/example/toebeanz/MainActivity.kt
+++ app/src/main/java/com/example/toebeanz/MainActivity.kt
@@ -7,5 +7,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Define needed variables
+        var random = (0..3).random()
+        val fourColors = arrayOf("Green", "Yellow", "Blue", "Red")
+        val allColors: ArrayList<String> = arrayListOf(fourColors[random])
+        val start = findViewById<Button>(R.id.startBtn)
+        val activitiesArray = arrayOf(Green.)
     }
 }
