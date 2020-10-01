efd5b5b0d8455d6535fdf33e8a5aa6ef0eeb3fec
Hannah <hannahstone4@hotmail.com>
2020-10-01 16:16:47 -0300

    Worked more on game itself.

--- app/src/main/java/com/example/toebeanz/Green.kt
+++ app/src/main/java/com/example/toebeanz/Green.kt
@@ -2,10 +2,26 @@ package com.example.toebeanz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class Green : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_green)
+
+        // Fetch buttons and textviews
+        val title = findViewById<TextView>(R.id.titleText)
+        val scoreText = findViewById<TextView>(R.id.scoreText)
+        val green = findViewById<Button>(R.id.greenBtn)
+        val yellow = findViewById<Button>(R.id.yellowBtn)
+        val blue = findViewById<Button>(R.id.blueBtn)
+        val red = findViewById<Button>(R.id.redBtn)
+        val restart = findViewById<Button>(R.id.restartBtn)
+
+        //Get count, index and colour from intent
+        var score= intent.getIntExtra("score", -2)
+        var count =intent.getIntExtra("count", -3)
+        val colors = intent.getStringArrayListExtra("colors")
     }
 }
--- app/src/main/java/com/example/toebeanz/MainActivity.kt
+++ app/src/main/java/com/example/toebeanz/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.toebeanz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +15,23 @@ class MainActivity : AppCompatActivity() {
         val fourColors = arrayOf("Green", "Yellow", "Blue", "Red")
         val allColors: ArrayList<String> = arrayListOf(fourColors[random])
         val start = findViewById<Button>(R.id.startBtn)
-        val activitiesArray = arrayOf(Green.)
+        val activitiesArray = arrayOf(Green::class.java)
+
+        // Colours Toe Beanz
+        for(i in 0..3){
+            random = (0..3).random()
+            allColors.add(fourColors[random])
+        }
+
+        // Start button used to begin game
+
+        start.setOnClickListener{
+            val intent = Intent(this@MainActivity, Green::class.java)
+            intent.putStringArrayListExtra("colors", allColors)
+            intent.putExtra("count", 0)
+            intent.putExtra("score", 0)
+            startActivity(intent)
+        }
+
     }
 }
